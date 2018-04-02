0ba407fea383b0dc9ec67c58ab3533857ee1a0ac
tuanpmt38 <37221796+tuanpmt38@users.noreply.github.com>
2018-04-02 13:59:59 +0700

    pf-java-oop Shape

--- ShapeTest.java
+++ ShapeTest.java
@@ -1,5 +1,29 @@
 public class ShapeTest {
     public static void main(String[] args) {
+//        Circle circle = new Circle();
+//        System.out.println(circle);
+//
+//        circle = new Circle(3.5);
+//        System.out.println(circle);
+//
+//        circle = new Circle("indigo", true, 3.5);
+//        System.out.println(circle);
+//
+//        Rectangle rectangle = new Rectangle();
+//        System.out.println(rectangle);
+//
+//        rectangle = new Rectangle(2.3, 5.8);
+//        System.out.println(rectangle);
+//
+//        rectangle = new Rectangle(2.5, 3.8, "orange", true);
+//        System.out.println(rectangle);
+        Square square = new Square();
+        System.out.println(square);
 
+        square = new Square(2.3);
+        System.out.println(square);
+
+        square = new Square(5.8, "yellow", true);
+        System.out.println(square);
     }
 }
